55aa3ce5663903451038f89a1612f13f702b5c6d
ysf <1142669510@qq.com>
2024-11-18 11:48:09 +0800

    单文件组件

---
+++ 19.单文件组件/School.vue
@@ -0,0 +1,34 @@
+<template>
+   <!--  组件结构 -->
+    <div class="demo">
+      <h2>学校名称：{{name}} </h2>
+      <h2>学校地址：{{address}}</h2>
+      <button @click="showName">点我提示学校名</button>
+    </div>
+</template>
+
+<script>
+  // 组件交互相关代码（数据、方法）
+    export default {
+    name:'School',
+    data() {
+      return {
+        name: "vue",
+        address: "北京",
+      };
+    },
+    methods: {
+      showName() {
+        console.log("showName", this);
+      },
+    },
+  }
+
+</script>
+
+<style> 
+ /*  组件的样式 */
+  .demo{
+    background-color: orange;
+  }
+</style>
---
+++ 19.单文件组件/Student.vue
@@ -0,0 +1,20 @@
+<template>
+   <!--  组件结构 -->
+    <div >
+      <h2>学生名称：{{name}} </h2>
+      <h2>学生年龄：{{age}}</h2>
+    </div>
+</template>
+
+<script>
+  // 组件交互相关代码（数据、方法）
+    export default {
+    name:'Student',
+    data() {
+      return {
+        name: "vue",
+        age: "18",
+      };
+    },
+  }
+</script>
